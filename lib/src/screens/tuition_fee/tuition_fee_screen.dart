b8bebb6e20e63c8a2be0c3dee888641f2cf8acd7
tamilarasan-official <your-email@example.com>
2026-07-22 14:16:31 +0530

    Initial Flutter frontend setup

---
+++ lib/src/screens/tuition_fee/tuition_fee_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TuitionFeeScreen extends StatelessWidget {
+  const TuitionFeeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Tuition Fee'),
+      ),
+    );
+  }
+}
