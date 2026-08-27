f737a4b9307f8c4434ba6a0b7f3c7d0070bff1c2
Vishnu <aanalystvishu@gmail.com>
2026-08-27 20:16:47 +0530

    feat: move student wallets into alphabetical directory

--- test/accountant_wallet_screen_test.dart
+++ test/accountant_wallet_screen_test.dart
@@ -21,8 +21,12 @@ void main() {
     );
     await tester.pumpAndSettle();
 
+    expect(find.text('Abinaya S'), findsNothing);
+    await tester.tap(find.byKey(const ValueKey('open-student-wallets')));
+    await tester.pumpAndSettle();
+
     expect(find.text('Abinaya S'), findsOneWidget);
-    expect(find.text('120'), findsNWidgets(2));
+    expect(find.text('120'), findsOneWidget);
 
     await tester.tap(find.byKey(const ValueKey('credit-student-1')));
     await tester.pumpAndSettle();
@@ -34,7 +38,7 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(repository.creditedAmount, 250);
-    expect(find.text('370'), findsNWidgets(2));
+    expect(find.text('370'), findsOneWidget);
     expect(find.textContaining('250 credits added'), findsOneWidget);
   });
 }
