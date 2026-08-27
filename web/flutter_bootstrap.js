a541bbb7eb6b2405383bc15c2f79c7a6bae0a39c
Vishnu <aanalystvishu@gmail.com>
2026-08-27 22:28:43 +0530

    refine gatepass QR module card

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -12,7 +12,7 @@
 // entry point as well so a deployment always paints the UI it contains.
 for (const finalBuild of _flutter.buildConfig.builds) {
   if (finalBuild.mainJsPath != null) {
-    finalBuild.mainJsPath = 'main.dart.js?v=20260827-13';
+    finalBuild.mainJsPath = 'main.dart.js?v=20260827-14';
   }
 }
 _flutter.loader.load();
